7b11f8418db8b311cdfaa10cf0ea618ec87543f9
Anna Raptidou Cole <acrrj123@gmail.com>
2023-12-28 15:52:38 -0600

    Lab done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def get_page_count(self):
+        return self._page_count
     
-        
+    def set_page_count(self, page_count):
+        if type(page_count) in (int, float):
+            self._page_count = page_count
+        else: 
+            print('page_count must be an integer')
+
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+book = Book("And Then There Were None", 272)
+print(book.page_count)  
+print(book.title)  
+
+      
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, condition='New'):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if type(size) in (int, float):
+            self._size = size
+        else: 
+            print("size must be an integer")
+
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        print('Your shoe is as good as new!')
+        
+# shoe = Shoe('Nike', 9, 'worn')
+# print(shoe.condition)
+    
+
+  
+            
